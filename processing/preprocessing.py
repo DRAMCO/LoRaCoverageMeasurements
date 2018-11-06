61e5037e5898cd1f8a5e6d211029469f5bdfd420
Gilles Callebaut <callebaut.gilles@gmail.com>
2018-11-06 22:27:53 +0100

    automate preprocessing based on config file

--- processing/preprocessing.py
+++ processing/preprocessing.py
@@ -19,9 +19,9 @@
                  - concatenates different raw csv files
                  - filters data (e.g., values withouth GPS)
                  - removes unneeded data
-                 - sorts by time 
+                 - sorts by time
                  - distance to transmitters (CENTER)
-                 - PL 
+                 - PL
                 Stores a pickle file in ../result
 """
 
@@ -32,58 +32,55 @@ import numpy as np
 import pandas as pd
 
 import util as util
+import json
 
-HEADER_LONG = ["time", "sat", "satValid", "hdopVal", "hdopValid", "vdopVal", "pdopVal", "lat", "lon", "locValid", "age",
-               "ageValid", "alt", "altValid", "course", "courseValid", "speed", "speedValid", "rssi", "snr", "freqError",  "sf", "isPacket"]
-HEADER_SHORT = ["time", "sat", "satValid", "hdopVal", "hdopValid", "vdopVal", "pdopVal", "lat", "lon", "locValid", "age",
-          "ageValid", "alt", "altValid", "rssi", "snr",  "sf", "isPacket"]
-
+HEADER = ["time", "sat", "satValid", "hdopVal", "hdopValid", "vdopVal", "pdopVal", "lat", "lon", "locValid", "age",
+          "ageValid", "alt", "altValid", "course", "courseValid", "speed", "speedValid", "rssi", "snr", "freqError",  "sf", "isPacket"]
 
 currentDir = os.path.dirname(os.path.abspath(__file__))
-current_path = os.path.abspath(os.path.join(
-    currentDir, '..', 'data', 'measurements_2'))
-all_files = glob.glob(os.path.join(current_path, "*.csv"))
+path_to_measurements = os.path.abspath(os.path.join(
+    currentDir, '..', 'data'))
+output_path=os.path.abspath(os.path.join(
+                currentDir, '..', 'result'))
 
-print("--------------------- EXECUTING preprocessing.py ---------------------")
-size = 0
-for file in all_files:
-    size += os.path.getsize(file)
-print(" Reading {0} files {1:.2f} kB".format(len(all_files), size/(1024)))
+with open(os.path.join(path_to_measurements, "measurements.json")) as f:
+    config = json.load(f)
+    measurements = config["measurements"]
+    for measurement in measurements:
+        path_to_measurement = os.path.join(path_to_measurements, measurement, 'all')
+        all_files=glob.glob(os.path.join(path_to_measurement, "*.csv"))
 
-df_from_each_file = []
-for f in all_files:
-    df = pd.read_csv(f, sep=',', header=None,
-                     names=None)
-    df.columns = HEADER_LONG if(
-        df.shape[1] == len(HEADER_LONG)) else HEADER_SHORT
-    df_from_each_file.append(df)
+        print("--------------------- PREPROCESSING {} ---------------------".format(measurement))
+        size=0
+        for file in all_files:
+            size += os.path.getsize(file)
+        print(" Reading {0} files {1:.2f} kB".format(len(all_files), size/(1024)))
 
-df = pd.concat(df_from_each_file, ignore_index=True, sort=False)
+        df_from_each_file=(pd.read_csv(f, sep=',', header=None,
+                                         names=HEADER) for f in all_files)
+        df=pd.concat(df_from_each_file, ignore_index=True)
+        total_rows=df.shape[0]
 
-total_rows = df.shape[0]
+        df=util.filter(df)
 
-df = util.filter(df)
-df['time'] = pd.to_datetime(
-    df['time'], format='%m/%d/%Y %H:%M:%S ', utc=True)
+        conf_file=os.path.join(path_to_measurement, 'conf.json')
 
-df.sort_values(by='time')
+        CENTER = config[measurement]["center"]
 
-CENTER = [51.0595576, 3.7085241]
-util.addDistanceTo(df, CENTER)
-df = df[df['distance'] < 10*1000]
-util.addPathLossTo(df)
 
-current_rows = df.shape[0]
-current_rows_data = df[df.isPacket > 0].shape[0]
+        util.addDistanceTo(df, CENTER)
+        df=df[df['distance'] < 1000]
+        util.addPathLossTo(df)
+        current_rows=df.shape[0]
+        current_rows_data=df[df.isPacket > 0].shape[0]
+        print(" Processed {0}/{1} {2:.1f}% rows".format(current_rows,
+              total_rows, (current_rows/total_rows)*100))
+        print(" Processed {0}/{1} {2:.1f}% and {3:.1f}% of total".format(current_rows_data,
+              current_rows, (current_rows_data/current_rows)*100, (current_rows_data/total_rows)*100))
+        
+        output_file="preprocessed_data.pkl"
+        output_file_path=os.path.join(output_path, measurement, output_file)
+        df.to_pickle(output_file_path)
 
-print(" Processed {0}/{1} {2:.1f}% rows".format(current_rows, total_rows, (current_rows/total_rows)*100))
-print(" Processed {0}/{1} {2:.1f}% and {3:.1f}% of total".format(current_rows_data, current_rows, (current_rows_data/current_rows)*100,(current_rows_data/total_rows)*100))
-
-output_path = os.path.abspath(os.path.join(
-    currentDir, '..', 'result'))
-output_file = "preprocessed_data_{}.pkl".format(
-    df['time'].dt.strftime("%Y_%m_%d").iloc[0])
-output_file_path = os.path.join(output_path, output_file)
-df.to_pickle(output_file_path)
-print(" Saving to {}".format(output_file_path))
-print("--------------------- DONE preprocessing.py ---------------------")
+        print(" Saving to {}".format(output_file_path))
+        print("--------------------- DONE preprocessing.py ---------------------")
